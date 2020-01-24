c8acbea2c6027094628106d09670fc8652249b73
Phillip Jones <ptj92e@gmail.com>
2020-01-24 09:56:12 -0600

    Added switch statement

--- employee.js
+++ employee.js
@@ -3,21 +3,84 @@ let inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "",
     database: "employeeDB"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     manageEmployee();
-  });
+});
 
-  
+function manageEmployee() {
+    inquirer.prompt({
+        name: "selection",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "View all roles",
+            "Add a new employee",
+            "Add a new department",
+            "Add a new role",
+            "Update an employee's role",
+            "Assign an employee a manager",
+            "View all manager's employees",
+            "Delete an employee",
+            "Delete a department",
+            "Delete a role",
+            "View a department's utilized budget"
+        ]
+    }).then(function (answer) {
+        switch (answer.selection) {
+            case "View all employees":
+                viewEmployees();
+                break;
+            case "View all departments":
+                viewDepartments();
+                break;
+            case "View all roles":
+                viewroles();
+                break;
+            case "Add a new employee":
+                addEmployee();
+                break;
+            case "Add a new department":
+                addDepartment();
+                break;
+            case "Add a new role":
+                addRole();
+                break;
+            case "Update an employee's role":
+                updateRole();
+                break;
+            case "Assign an employee a manager":
+                assignManager();
+                break;
+            case "View all manager's employees":
+                viewManEmp();
+                break;
+            case "Delete an employee":
+                deleteEmployee();
+                break;
+            case "Delete a department":
+                deleteDepartment();
+                break;
+            case "Delete a role":
+                deleteRole();
+                break;
+            case "View a department's utilized budget":
+                viewBudget();
+                break;
+        }
+    })
+}
