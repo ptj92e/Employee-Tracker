8e9736b3e4e057e1cae149a09dd6b47805cf90c6
Phillip Jones <ptj92e@gmail.com>
2020-02-06 09:03:01 -0600

    Comment code

--- employee.js
+++ employee.js
@@ -1,6 +1,7 @@
+//Requiring mysql and inquirer for the app to work
 let mysql = require("mysql");
 let inquirer = require("inquirer");
-
+//This establishes a connection to the database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -8,18 +9,18 @@ var connection = mysql.createConnection({
     port: 3306,
 
     // Your username
-    user: "ptj92e",
+    user: "",
 
     // Your password
-    password: "novaPuppy216",
+    password: "",
     database: "employeeDB"
 });
-
+//This sets up the connection and runs the manageEmployee function
 connection.connect(function (err) {
     if (err) throw err;
     manageEmployee();
 });
-
+//This function is a one prompt the includes a switch statement to point the application in the direction of which method of run next
 function manageEmployee() {
     inquirer.prompt({
         name: "selection",
@@ -84,7 +85,7 @@ function manageEmployee() {
         }
     });
 };
-
+//This function pulls all of the information from the mySQL database through a series of inner joins and a left join. Once the information has been pulled, I used console.table to create a clear table of all of the information gathered
 function viewEmployees() {
     connection.query("SELECT CONCAT(e.first_name, ' ', e.last_name) AS employee, r.title, r.salary, d.name, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee AS e INNER JOIN role AS r ON r.id = e.role_id INNER JOIN department AS d ON d.id = r.department_id LEFT JOIN employee AS m ON m.id = e.manager_id;", function (err, data) {
         if (err) throw err;
@@ -95,7 +96,7 @@ function viewEmployees() {
         manageEmployee();
     });
 };
-
+//This function functions similarly to the viewEmployees but there is an extra step. The initial connection pulls the names of the departments from the database and turns them into a list of answer choices. From there, all of the employees with the department of the user's choice are put into a table to be viewed. 
 function viewDepartments() {
     connection.query("SELECT * FROM department", function (err, result) {
         if (err) throw err;
@@ -118,7 +119,7 @@ function viewDepartments() {
         });
     });
 };
-
+//This function does the same as the viewDepartments function except with the roles
 function viewRoles() {
     connection.query("SELECT * FROM role;", function (err, result) {
         if (err) throw err;
@@ -141,7 +142,7 @@ function viewRoles() {
         });
     });
 };
-
+//This funciton uses a series of inquirer promts to ask the user for the employee's name and to choose their role from a list of roles.
 function addEmployee() {
     connection.query("SELECT * FROM role", function (err, result) {
         if (err) throw err;
@@ -164,7 +165,7 @@ function addEmployee() {
                 choices: choices
             }
         ]).then(function (data) {
-
+            //This inputs the role's id into the query so the employee can be created into the database
             let roleID = "";
             for (let i = 0; i < result.length; i++) {
                 if (data.role === result[i].title) {
@@ -177,7 +178,7 @@ function addEmployee() {
         });
     });
 };
-
+//This function works similarly to the addEmployee function  and only takes in the name of the department as a parameter
 function addDepartment() {
     inquirer.prompt({
         name: "name",
@@ -189,7 +190,7 @@ function addDepartment() {
         manageEmployee();
     });
 };
-
+//This function adds a role but asks for a the title of the role, the salary of that role, and the department which it belongs to. 
 function addRole() {
 
     connection.query("SELECT * FROM department", function (err, result) {
@@ -213,6 +214,7 @@ function addRole() {
                 choices: choices
             }
         ]).then(function (data) {
+            //This anonymous function turns the department's id into a variable and is then passed into the query to create the new role. 
             let departmentID = "";
             for (let i = 0; i < result.length; i++) {
                 if (data.department === result[i].name) {
@@ -225,7 +227,7 @@ function addRole() {
         });
     });
 };
-
+//This function uses two different connections to pull a list of employees and a list of roles. Those are turned into answer choices for the inquirer prompt. 
 function updateRole() {
     connection.query("SELECT * FROM role;", function (err, result) {
         if (err) throw err;
@@ -247,6 +249,7 @@ function updateRole() {
                     choices: roles
                 }
             ]).then(function (data) {
+                //This anonymous function determines whether or not the role selected is the current role of the rmployee. If it is, the user is alerted and the function starts over. If it is not, the employee's role is updated
                 for (let i = 0; i < employees.length; i++) {
                     if (data.employee === employees[i] && data.role === info[i].title) {
                         console.log("That is already this employee's role.");
@@ -269,7 +272,7 @@ function updateRole() {
         });
     });
 };
-
+//This function assigns a manager to an employee. The first connection gathers a list of employees to use as answer choices. 
 function assignManager() {
     connection.query("SELECT * FROM employee;", function(err, data) {
         if (err) throw err;
@@ -288,6 +291,7 @@ function assignManager() {
                 choices: employees
             }
         ]).then(function(result) {
+            //This section of the application checks the name of the employee and the manager selected. For the manager selected, it takes their id and sets it as the manager id for the employee who is being assigned a manager
             let employeeName = result.employee.split(" ");
             let employeeFirst = employeeName[0];
             let employeeLast = employeeName[1];
@@ -306,7 +310,7 @@ function assignManager() {
         });
     });
 };
-
+//This connection uses two inner joins and a left join to retrieve information based off of which manager whas selected. It populates all of the employees for that manager from the choices
 function viewManEmp() {
     connection.query("SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS employee, e.manager_id, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee AS e LEFT JOIN employee AS m ON m.id = e.manager_id", function(err, data) {
         if (err) throw err;
@@ -337,7 +341,7 @@ function viewManEmp() {
         });
     });
 };
-
+//This function deletes an employee from the database based off of which choice was selected
 function deleteEmployee() {
     connection.query("SELECT * FROM employee;", function (err, data) {
         if (err) throw err;
@@ -357,7 +361,7 @@ function deleteEmployee() {
         });
     });
 };
-
+//This functions similarly to deleteEmployee except when a department is deleted, it cascades and deletes each role and employee currently assigned to the department which was deleted
 function deleteDepartment() {
     connection.query("SELECT * FROM department", function(err, data) {
         if (err) throw err;
@@ -374,7 +378,7 @@ function deleteDepartment() {
         });
     });
 };
-
+//This functions similarly to delete employee but it also deletes each employee that shares the role that is selected
 function deleteRole() {
     connection.query("SELECT * FROM role", function(err, data) {
         if (err) throw err;
@@ -391,8 +395,9 @@ function deleteRole() {
         });
     });
 };
-
+//This function displays the used budget of the department that was selected
 function viewBudget() {
+    //This creates the answer choices
     connection.query("SELECT * FROM department", function(err, result){
         if (err) throw err;
         let departments = result.map(function(department) {return department.name});
@@ -402,9 +407,12 @@ function viewBudget() {
             message: "Which department's utilized budget would you like to view?",
             choices: departments
         }).then(function(info) {
+            //This query pulls salary information from the database
             connection.query("SELECT r.title, r.salary, d.name, e.first_name, e.last_name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id INNER JOIN employee as e ON e.role_id = r.id WHERE d.name = ?;", [info.department], function(err, data) {
                 if (err) throw err;
+                //The salary information is then put into an array
                 let salaries = data.map(function(employee) {return employee.salary});
+                //The reduce method takes the array and adds index 0, and 1 of the array together until there is only one value in the array. This is saved into a variable called budget
                 let budget = salaries.reduce(function(a, b) {return a + b});
                 console.log("The " + info.department + " department has used $" + budget + ".");
                 manageEmployee();
